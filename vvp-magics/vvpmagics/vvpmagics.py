e91e6514c6630280c10a4b22e1a9cea32c1f87d1
Scott Thomson <scott.thomson@tngtech.com>
2020-06-09 16:17:11 +0200

    #2 Add requirements.txt. Make magics interact with namespaces.

--- vvp-magics/vvpmagics/vvpmagics.py
+++ vvp-magics/vvpmagics/vvpmagics.py
@@ -1,15 +1,29 @@
 from IPython.core.magic import (Magics, magics_class, line_magic)
+import requests
 
 print('Loading vvp-vvpmagics.')
 
 
 @magics_class
 class VvpMagics(Magics):
-
-    vvpUrl = "http://localhost:8000"
+    vvpBaseUrl = ""
+    namespacesEndpoint = "/namespaces/v1/namespaces"
 
     @line_magic
-    def getConfig(self, line):
-        # TODO #2: Do the connection
-        return "Will return the config from {}/config.json .".format(self.vvpUrl)
+    def connect_vvp(self, line, hostname, port, namespace):
+        self.vvpBaseUrl = "https://{}:{}".format(hostname, port)
+        if not (hostname & port):
+            raise Exception("A hostname and port were not specified.")
+        if not namespace:
+            return self._get_namespaces()
+        return self._get_namespace(namespace)
 
+    def _get_namespaces(self):
+        url = self.vvpBaseUrl.append(self.namespacesEndpoint)
+        request = requests.get(url)
+        return request.text
+
+    def _get_namespace(self, namespace):
+        url = self.vvpBaseUrl.append(self.namespacesEndpoint).append("/{}".format(namespace))
+        request = requests.get(url)
+        return request.text
