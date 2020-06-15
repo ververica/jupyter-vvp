f66cefb7ae39589cf30a0867aa422a08baa6795a
Scott Thomson <scott.thomson@tngtech.com>
2020-06-15 16:45:13 +0200

    #2 Use mocks in test_vvpmagics.py.

--- vvp-magics/test/test_vvpmagics.py
+++ vvp-magics/test/test_vvpmagics.py
@@ -1,20 +1,34 @@
+import unittest
+
+import requests_mock
+
 from vvpmagics import VvpMagics
 
 
-def test_connect_vvp_calls_namespaces_endpoint_if_none_given():
-    magic_line = "localhost -p 8080"
-    magics = VvpMagics()
+@requests_mock.Mocker()
+class VvpMagicsTests(unittest.TestCase):
 
-    response = magics.connect_vvp(magic_line)
+    def test_connect_vvp_calls_namespaces_endpoint_if_none_given(self, requests_mock):
+        requests_mock.request(method='get', url='http://localhost:8080/namespaces/v1/namespaces',
+                              text="""
+        { "namespaces": [{ "name": "namespaces/default" }] }
+        """)
 
-    assert not len(response['namespaces']) == 0
+        magic_line = "localhost -p 8080"
+        magics = VvpMagics()
 
+        response = magics.connect_vvp(magic_line)
 
-def test_connect_vvp_creates_session_if_namespace_given():
-    magic_line = "localhost -n default"
-    magics = VvpMagics()
+        assert not len(response['namespaces']) == 0
 
-    session = magics.connect_vvp(magic_line)
+    def test_connect_vvp_creates_session_if_namespace_given(self, requests_mock):
+        requests_mock.request(method='get', url='http://localhost:8080/namespaces/v1/namespaces/default',
+                              text="""
+        { "namespace": [{ "name": "namespaces/default" }] }
+        """)
+        magic_line = "localhost -n default"
+        magics = VvpMagics()
 
-    assert session.get_namespace() == "default"
+        session = magics.connect_vvp(magic_line)
 
+        assert session.get_namespace() is not None
