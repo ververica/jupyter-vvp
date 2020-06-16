91fa4d48823fb6afa09a98f20ba3b035175ad2b8
Scott Thomson <scott.thomson@tngtech.com>
2020-06-16 11:20:19 +0200

    #2 Put all Magics in same class.

--- vvp-magics/vvpmagics/vvpmagics.py
+++ vvp-magics/vvpmagics/vvpmagics.py
@@ -1,6 +1,6 @@
 import json
 import requests
-from IPython.core.magic import (Magics, magics_class, line_magic)
+from IPython.core.magic import (Magics, magics_class, line_magic, cell_magic)
 from IPython.core.magic_arguments import magic_arguments, argument, parse_argstring
 
 from vvpmagics.vvpsession import VvpSession
@@ -34,4 +34,19 @@ class VvpMagics(Magics):
         request = requests.get(url)
         namespaces = json.loads(request.text)
         return namespaces
+
+    @cell_magic
+    @magic_arguments()
+    @argument('session', type=str, help='Name of the object representing the connection to a given vvp namespace.')
+    def execute_catalog_statement(self, line, cell):
+        args = parse_argstring(self.execute_catalog_statement, line)
+        session = self.shell.user_ns[args.session]
+        catalog_endpoint = "/catalog/v1beta1/namespaces/{}:execute" \
+            .format(session.get_namespace())
+        if cell:
+            response = session.submit_post_request(catalog_endpoint, cell)
+            print(response)
+            print(response.text)
+        else:
+            print("Empty cell: doing nothing.")
 
