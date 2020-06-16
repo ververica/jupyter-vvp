4c7d32d0dfbf7a1654148c9dd3d83ef127a934c2
Scott Thomson <scott.thomson@tngtech.com>
2020-06-16 17:18:11 +0200

    #3 Make sessions easier to use.

--- vvp-magics/vvpmagics/vvpsession.py
+++ vvp-magics/vvpmagics/vvpsession.py
@@ -6,8 +6,8 @@ namespaces_endpoint = "/namespaces/v1/namespaces"
 
 
 class VvpSession:
-    sessions = {}
-    default_session = None
+    _sessions = {}
+    default_session_name = None
 
     def __init__(self, vvp_base_url: str, namespace: str):
         """
@@ -23,23 +23,31 @@ class VvpSession:
             raise Exception("Invalid or empty namespace specified.")
         self._namespace = namespace
 
-    @staticmethod
-    def get_sessions():
-        return VvpSession.sessions.keys()
+    @classmethod
+    def get_sessions(cls):
+        return cls._sessions.keys()
+
+    @classmethod
+    def get_session(cls, session_name=None):
+        try:
+            return cls._sessions[session_name or cls.default_session_name]
+        except KeyError:
+            raise Exception("The session name {} is invalid.".format(session_name))
 
     @classmethod
     def create_session(cls, vvp_base_url, namespace, session_name, set_default=False, force=False):
         session = cls(vvp_base_url, namespace)
         cls._add_session_to_dict(session_name, session, force=force)
-        if (cls.default_session is None) or set_default:
-            cls.default_session = session
+        if (cls.default_session_name is None) or set_default:
+            cls.default_session_name = session_name
         return session
 
     @classmethod
     def _add_session_to_dict(cls, session_name, session, force=False):
-        if (session_name in cls.sessions) and not force:
-            raise Exception("The session name already exists. Please use --force to update.")
-        cls.sessions[session_name] = session
+        if (session_name in cls._sessions) and not force:
+            raise Exception("The session name {} already exists. Please use --force to update."
+                            .format(session_name))
+        cls._sessions[session_name] = session
 
     def get_namespace(self):
         return self._namespace
@@ -59,6 +67,13 @@ class VvpSession:
         request = self._http_session.get(namespaces_endpoint + "/{}".format(namespace))
         namespace = (json.loads(request.text))["namespace"]
         return namespace
+
+    @staticmethod
+    def get_namespaces(base_url):
+        print("Requesting from {}...".format(base_url + namespaces_endpoint))
+        request = HttpSession(base_url, None).get(namespaces_endpoint)
+        namespaces = json.loads(request.text)
+        return namespaces
 
     def submit_post_request(self, endpoint, requestbody):
         request = self._http_session.post(
