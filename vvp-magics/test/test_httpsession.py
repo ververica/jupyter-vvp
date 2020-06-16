4ba44a2e86b311273ac29f59fcd64eaac13014f9
Scott Thomson <scott.thomson@tngtech.com>
2020-06-16 09:24:09 +0200

    #2 Ensuring that test_httpsession.py runs.

--- vvp-magics/test/test_httpsession.py
+++ vvp-magics/test/test_httpsession.py
@@ -7,26 +7,28 @@ from vvpmagics.httpsession import HttpSession
 @requests_mock.Mocker()
 class HttpSessionTests(unittest.TestCase):
     baseurl = "http://localhost:8080"
-    path = "test"
-    result = "ok"
+    path = "/test"
+    return_text = "Hello, world."
 
     def test_get_request(self, requests_mocker):
 
         requests_mocker.request(
-            method='get', url='http://localhost:8080/{}'.format(self.path), text=self.result)
+            method='get', url='http://localhost:8080{}'.format(self.path), text=self.return_text)
 
-        session = HttpSession(self.baseurl, None)
+        result = HttpSession(self.baseurl, None).get(self.path)
 
-        assert session.get(self.path) == self.result
+        assert result.status_code == 200
+        assert result.text == self.return_text
 
     def test_post_request(self, requests_mocker):
         def match_request_text(request):
-            # request.text may be None, or '' prevents a TypeError.
             return 'test' in (request.text or '')
 
-        requests_mocker.request(method='post', url='http://localhost:8080//{}'.format(
-            self.path), additional_matcher=match_request_text, text=self.result)
+        requests_mocker.request(method='post', url='http://localhost:8080{}'.format(
+            self.path), additional_matcher=match_request_text, text=self.return_text)
 
-        session = HttpSession(self.baseurl, None)
+        result = HttpSession(self.baseurl, None).post(self.path, "hello world test")
 
-        assert session.post(self.path, "hello world test") == self.result
+        assert result.status_code == 200
+        assert result.text == self.return_text
+
