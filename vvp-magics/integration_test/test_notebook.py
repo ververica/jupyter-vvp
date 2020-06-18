2dfc856bbe3e13719f06188b76d618a0ebbb40b1
Michael Pisula <michael.pisula@tngtech.com>
2020-06-18 08:08:19 +0200

    add jupyter integration test

---
+++ vvp-magics/integration_test/test_notebook.py
@@ -0,0 +1,41 @@
+import os
+import unittest
+
+import nbformat
+from nbconvert.preprocessors import ExecutePreprocessor
+
+from vvpmagics import vvpmagics
+
+def run_notebook(notebook_path):
+    nb_name, _ = os.path.splitext(os.path.basename(notebook_path))
+    dirname = os.path.dirname(notebook_path)
+
+    with open(notebook_path) as f:
+        nb = nbformat.read(f, as_version=4)
+
+    proc = ExecutePreprocessor(timeout=600)
+    proc.allow_errors = True
+
+    proc.preprocess(nb, {'metadata': {'path': '/'}})
+    output_path = os.path.join(dirname, '{}_all_output.ipynb'.format(nb_name))
+
+    with open(output_path, mode='wt') as f:
+        nbformat.write(nb, f)
+    errors = []
+    for cell in nb.cells:
+        if 'outputs' in cell:
+            for output in cell['outputs']:
+                if output.output_type == 'error':
+                    errors.append(output)
+    return nb, errors
+
+
+class IntegrationTest(unittest.TestCase):
+
+    def test_connect_notebook(self):
+        nb, errors = run_notebook('ConnectToVVP.ipynb')
+        self.assertEqual(errors, [])
+
+
+if __name__ == '__main__':
+    unittest.main()
