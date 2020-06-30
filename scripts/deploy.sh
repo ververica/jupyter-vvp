97a98c23c6a29421d7bd032ce9d29e4f0797e08e
Michael Pisula <michael.pisula@tngtech.com>
2020-06-30 08:15:04 +0200

    put deployment into deploy step again

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-docker push vvp-jupyter:latest
+openssl aes-256-cbc -K $encrypted_6d931d8515f6_key -iv $encrypted_6d931d8515f6_iv -in ./gcp-client-secret.json.enc -out $HOME/gcp-client-secret.json -d
+gcloud --quiet auth activate-service-account --key-file $HOME/gcp-client-secret.json
+sudo ln -s /usr/lib/google-cloud-sdk/bin/docker-credential-gcloud /usr/bin/docker-credential-gcloud
+gcloud --quiet auth configure-docker
+docker push eu.gcr.io/vvp-devel-240810/vvp-jupyter:latest
