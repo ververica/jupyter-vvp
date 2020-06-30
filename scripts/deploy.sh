9263d98d31c9c94ca2be0dbd6f4be1cb6080c20d
Michael Pisula <michael.pisula@tngtech.com>
2020-06-30 08:57:23 +0200

    make deploy produce more output

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
 openssl aes-256-cbc -K $encrypted_6d931d8515f6_key -iv $encrypted_6d931d8515f6_iv -in ./gcp-client-secret.json.enc -out $HOME/gcp-client-secret.json -d
 gcloud --quiet auth activate-service-account --key-file $HOME/gcp-client-secret.json
 sudo ln -s /usr/lib/google-cloud-sdk/bin/docker-credential-gcloud /usr/bin/docker-credential-gcloud
