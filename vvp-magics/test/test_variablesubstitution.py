45feceb851f74484cc81e7c794dc4ac1f2edb09a
Scott Thomson <scott.thomson@tngtech.com>
2020-07-07 16:44:22 +0200

    Allow double-brace variables to pass through substitution untouched.

--- vvp-magics/test/test_variablesubstitution.py
+++ vvp-magics/test/test_variablesubstitution.py
@@ -13,19 +13,31 @@ class VariableSubstitutionTests(unittest.TestCase):
     def setUp(self):
         self.ipython_session.run_cell(raw_cell='%reset -f')
 
-    def test_two_simple_variables_substituted(self):
+    def test_substitute_user_variables_works(self):
         ipython_session = self.ipython_session
 
-        input_text = "{var1} sat on {var2}."
-        ipython_session.run_cell(raw_cell='var1 = "The cat"')
-        ipython_session.run_cell(raw_cell='var2 = "the mat"')
-
+        input_text = """
+        INSERT INTO {{ namespace }}.{resultsTable}
+        SELECT * FROM {{ namespace }}.{tableName}
+        """
         formatter = VvpFormatter(input_text)
-        formatted = formatter.substitute_variables()
+        ipython_session.run_cell(raw_cell='resultsTable="table1"')
+        ipython_session.run_cell(raw_cell='tableName="table2"')
 
-        assert formatted == "The cat sat on the mat."
+        expected_output = """
+        INSERT INTO {{ namespace }}.table1
+        SELECT * FROM {{ namespace }}.table2
+        """
+        actual_output = formatter.substitute_user_variables()
+        assert actual_output == expected_output
 
-    def test_undefined_variable_throws(self):
+    def test_prepare_escaped_variables_works_in_simple_case(self):
+        input_text = "{{ variable }} and {{ another }} with { ignore }"
+        expected = "{{{{ variable }}}} and {{{{ another }}}} with { ignore }"
+
+        assert VvpFormatter._prepare_escaped_variables(input_text) == expected
+
+    def test_substitute_user_variables_undefined_variable_throws(self):
         ipython_session = self.ipython_session
 
         input_text = "{var1} sat on {var2}."
@@ -34,16 +46,30 @@ class VariableSubstitutionTests(unittest.TestCase):
         formatter = VvpFormatter(input_text)
 
         with self.assertRaises(VariableSubstitutionException) as exception:
-            formatter.substitute_variables()
+            formatter.substitute_user_variables()
             assert exception.variable_name == "var2"
 
-    def test_four_braces_transformed_to_two(self):
+    def test_substitute_variables_works_in_simple_case(self):
         ipython_session = self.ipython_session
 
-        input_text = "{var1} sat on {{{{ sittingObject }}}}."
+        input_text = "{var1} sat on {var2}."
+        escaped_text = input_text
+        ipython_session.run_cell(raw_cell='var1 = "The cat"')
+        ipython_session.run_cell(raw_cell='var2 = "the mat"')
+
+        formatter = VvpFormatter(input_text)
+        formatted = formatter._substitute_variables(escaped_text)
+
+        assert formatted == "The cat sat on the mat."
+
+    def test_substitute_variables_four_braces_transformed_to_two(self):
+        ipython_session = self.ipython_session
+
+        input_text = "{var1} sat on {{ sittingObject }}."
+        escaped_text = "{var1} sat on {{{{ sittingObject }}}}."
         ipython_session.run_cell(raw_cell='var1 = "The cat"')
 
         formatter = VvpFormatter(input_text)
-        formatted = formatter.substitute_variables()
+        formatted = formatter._substitute_variables(escaped_text)
 
         assert formatted == "The cat sat on {{ sittingObject }}."
--- vvp-magics/vvpmagics/variablesubstitution.py
+++ vvp-magics/vvpmagics/variablesubstitution.py
@@ -8,10 +8,18 @@ class VvpFormatter(string.Formatter):
     def __init__(self, input_string):
         self.input_string = input_string
 
-    def substitute_variables(self):
+    def substitute_user_variables(self):
+        escaped_string = self._prepare_escaped_variables(self.input_string)
+        return self._substitute_variables(escaped_string)
+
+    @staticmethod
+    def _prepare_escaped_variables(input_text):
+        return input_text.replace("{{ ", "{{{{ ").replace(" }}", " }}}}")
+
+    def _substitute_variables(self, escaped_string):
         try:
             session = get_ipython()
-            return self.input_string.format(**session.user_ns)
+            return escaped_string.format(**session.user_ns)
         except KeyError as error:
             raise VariableSubstitutionException(error,
                                                 message="The variable {} is not defined but is referenced in the cell."
